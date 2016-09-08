14597c9d306b850e6ae42fa507aed4d96251bdea
lancema <messi_18@live.cn>
2016-09-08 23:11:39 +0800

    exercise2.4 and 2.5

--- src/Chapter2.scala
+++ src/Chapter2.scala
@@ -4,4 +4,11 @@ object Chapter2 {
     def curry[A, B, C](f: (A, B) => C): A => (B => C) = (a: A) => (b: B) => f(a, b)
   }
 
+  object Exercise2_4 {
+    def uncurry[A, B, C](f: A => B => C): (A, B) => C = (a: A, b: B) => f(a)(b)
+  }
+
+  object Exercise2_5 {
+    def compose[A,B,C](f: A => B, g: B => C): A => C = (a:A) => g(f(a))
+  }
 }
