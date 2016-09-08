00d242ed75b40e7455b2bd8a4ac1abffd3606b7d
lancema <messi_18@live.cn>
2016-09-09 00:05:24 +0800

    tailrec annoted

--- src/Chapter2.scala
+++ src/Chapter2.scala
@@ -1,39 +1,38 @@
 import scala.annotation.tailrec
 
-object Chapter2{
+object Chapter2 {
 
-  object Exercise2_1{
-    def fib(n:Int):Int = {
+  object Exercise2_1 {
+    def fib(n: Int): Int = {
       @tailrec
-      def fib_inter(i:Int, p:Int, c:Int):Int = {
-        if(i < 1) 0
-        else if(i == 1) p
+      def fib_inter(i: Int, p: Int, c: Int): Int = {
+        if (i < 1) 0
+        else if (i == 1) p
         else if (i == 2) c
-        else fib_inter(i-1,c, p+c)
+        else fib_inter(i - 1, c, p + c)
       }
 
-      fib_inter(n,0,1)
+      fib_inter(n, 0, 1)
     }
   }
 
   object Exercise2_2 {
-    def isSorted[A](array:Array[A], f:(A,A) => Boolean): Boolean = {
+    def isSorted[A](array: Array[A], f: (A, A) => Boolean): Boolean = {
       val list = array.toList
-      def isSorted(l:List[A]):Boolean = {
-        l match {
-          case Nil => true
-          case _::Nil => true
-          case a::b::c =>
-            val r = f(a,b)
-            if (r){
-              r && isSorted(b::c)
-            } else {
-              false
-            }
-        }
+
+      @tailrec
+      def isSorted(prev: Boolean, l: List[A]): Boolean = {
+        if (!prev) false
+        else
+          l match {
+            case Nil => true
+            case _ :: Nil => true
+            case a :: b :: c =>
+              isSorted(f(a, b), b :: c)
+          }
       }
 
-      isSorted(list)
+      isSorted(true, list)
     }
   }
 
@@ -46,10 +45,10 @@ object Chapter2{
   }
 
   object Exercise2_5 {
-    def compose[A,B,C](f: A => B, g: B => C): A => C = (a:A) => g(f(a))
+    def compose[A, B, C](f: A => B, g: B => C): A => C = (a: A) => g(f(a))
   }
 
-  def main(args:Array[String]): Unit ={
+  def main(args: Array[String]): Unit = {
     assert(Exercise2_1.fib(0) == 0)
   }
 }
